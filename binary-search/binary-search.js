5284edfa4817e9fe7a5a567067f4b0a7b0ad1282
Kenneth Lai <klnyc6@gmail.com>
2019-09-05 16:35:29 -0400

    binary done

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,8 +2,43 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if (array.length === 1) {
+		if (array[0] === target) {
+			return true
+		} else {
+			return false
+		}
+	}
+	let midpoint = Math.ceil(array.length/2)
+	console.log(array, midpoint)
+	if (target < array[midpoint]) {
+		return binarySearch(array.slice(0, midpoint), target)
+	}
+	else if (target > array[midpoint]) {
+		return binarySearch(array.slice(midpoint), target)
+	} else {
+		return true
+	}
 };
+
+// const binarySearch = (array, target) => {
+//     let i = 0;
+//     let j = array.length;
+//     let mid = Math.ceil((i + j) / 2);
+//     while (i <= j){
+//         if (target < array[mid]) {
+//             j = mid - 1;
+//         }
+//         else if (target > array[mid]) {
+//             i = mid + 1;
+//         }
+//         else if (target === array[mid]){
+//             return true;
+//         }
+//         mid = Math.ceil((i + j) / 2);
+//     }
+//     return false;
+// };
 
 /*
 	EXTRA CREDIT:
